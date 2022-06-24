19cfc22f118c7f4e6366bf6245143b9dba0fcfbd
Patrick <patrick.laranjo.s@gmail.com>
2022-06-24 16:01:32 +0200

    Added git setup

---
+++ setup-git.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+##################################################################################################################
+
+# Problem solving commands
+
+# Read before using it.
+# https://www.atlassian.com/git/tutorials/undoing-changes/git-reset
+# git reset --hard orgin/master
+# ONLY if you are very sure and no coworkers are on your github.
+
+# Command that have helped in the past
+# Force git to overwrite local files on pull - no merge
+# git fetch all
+# git push --set-upstream origin master
+# git reset --hard orgin/master
+
+project=$(basename `pwd`)
+echo "-----------------------------------------------------------------------------"
+echo "this is project https://github.com/kunoros/"$project
+echo "-----------------------------------------------------------------------------"
+git config --global pull.rebase false
+git config --global user.name "Patrick Laranjo"
+git config --global user.email "patrick.laranjo.s@gmail.com"
+sudo git config --system core.editor nano
+#git config --global credential.helper cache
+#git config --global credential.helper 'cache --timeout=32000'
+git config --global push.default simple
+
+git remote set-url origin git@github.com:kunoros/$project
+
+echo "Everything set"
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
